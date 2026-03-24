93a01b8c87a451a79141475180494906cecb7e6f
Kyle Keller <kellerk563@gmail.com>
2026-03-23 23:03:41 -0500

    Add A11y menu and Theme Toggle projects.

---
+++ theme-toggle/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const themeToggle = document.getElementById('theme-toggle');
+	themeToggle.addEventListener('click', function() {
+		if (this.getAttribute('data-theme') === 'light') {
+			document.body.classList.remove('light');
+			document.body.classList.add('dark');
+			this.setAttribute('data-theme', 'dark');
+		} else {
+			document.body.classList.remove('dark');
+			document.body.classList.add('light');
+			this.setAttribute('data-theme', 'light');
+		}
+	});
+});
